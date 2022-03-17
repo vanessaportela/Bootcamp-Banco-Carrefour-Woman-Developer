b359f044327920a8eb216789bd7a7521d28ff6ae
vanessaportela <vgportela@outlook.com>
2022-03-17 20:23:27 -0300

    Ajuste do código

--- Desafio-Codigo9/ConsoleApp9/Program.cs
+++ Desafio-Codigo9/ConsoleApp9/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class TESTE
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        if (A >= 1 && A <= 40 && B >= 1 && B <= 40)
+        {
+            int C = A - B;
+            Console.WriteLine(C);
+        }
+    }
+}
